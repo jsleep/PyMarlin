fe84a3e8fdd038b3552cd9acd478be27a9a5dc5e
Felix M. Perez Quinones <felixperez78@hotmail.com>
2021-06-24 15:30:26 -0400

    Changed batch size in order for this to work.

--- examples/summarization/aml/submit_ortds.py
+++ examples/summarization/aml/submit_ortds.py
@@ -53,7 +53,7 @@ def get_args(outputSuffix="deepspeed_ort_amp_nopadding_v100_8"):
         '--config_path', 'config-prod.yaml',
         '--trainer.train_batch_size', 32,
         '--trainer.gpu_batch_size_limit', 32,
-        '--trainer.val_batch_size', 64,
+        '--trainer.val_batch_size', 32,
         '--trainer.epochs', 3,
         '--trainer.backend', "ddp-amp-apex",
         '--trainer.disable_tqdm', "true", # ugly logging in AML
